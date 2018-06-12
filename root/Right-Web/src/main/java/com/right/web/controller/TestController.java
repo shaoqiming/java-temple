9332eef681acb145758de7ecee29a3bf60d6e7e2
shaoqiming <549730265@qq.com>
2018-06-12 13:34:09 +0800

    添加日志，并将mapper移动到了dao项目中

--- root/Right-Web/src/main/java/com/right/web/controller/TestController.java
+++ root/Right-Web/src/main/java/com/right/web/controller/TestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.Right.Api.Entity.User;
 import org.Right.Api.Service.UserService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+	private static Logger logger=Logger.getLogger(TestController.class);
 
 	@Autowired
 	private UserService userService;
@@ -24,6 +26,9 @@ public class TestController {
 	@RequestMapping(value = "/getUserList.json")
 	@ResponseBody
 	public List<User> getUserList() {
+		logger.info("info");
+		logger.debug("debug");
+		logger.error("errot");
 		return userService.getUserList();
 	}
 
